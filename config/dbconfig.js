7b5618c8100b0b3658564f8e0cd8f11b9f544af5
Ahamed Thaiyub A <ahamedthaiyub94@gmail.com>
2025-06-17 23:42:41 +0530

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-exports.dbconnect = mongoose.connect('mongodb://localhost:27017/restaurant_mongo').then(console.log("connected")).catch((e)=>{console.log(e)}).finally("hahaa connected")
+exports.dbconnect = mongoose.connect('mongodb+srv://ahamedthaiyub27:1234@cluster0.wabhjcx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(console.log("connected")).catch((e)=>{console.log(e)}).finally("hahaa connected")
